4cc258ebc354ce14706286d07e863b83a3abc302
BuickSalesman <adam.odonnell.e@gmail.com>
2024-05-28 17:50:36 -0400

    coding practice

---
+++ 052824.js
@@ -0,0 +1,1 @@
+// did a hackerrank mock interview and some practice problems for about an hour today. The hackerrank interview made me feel very uneducated and underprepared, but we wil keep going.
