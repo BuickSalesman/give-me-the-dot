30a834bb704a67cd98fe8c3c559e6befea503f82
BuickSalesman <adam.odonnell.e@gmail.com>
2024-05-29 17:24:46 -0400

    coding practice

---
+++ 052924.rb
@@ -0,0 +1,3 @@
+# watched the below video on a falling sand project, learned a little bit about p5.js. Subscribed to both Coding Train and Fireship on youtube. I wonder if this is useful, but it kept me interesting in coding for the day. Hopefully tonight I can try my hand at the project.
+
+# https://www.youtube.com/watch?v=L4u7Zy_b868
